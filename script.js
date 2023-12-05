edbbbde61177b9249bd916fdb727a3b9bb5ba0d8
sagenothere <100663544+chelseaqnlyy@users.noreply.github.com>
2023-12-05 20:03:17 +0700

    script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const NoBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
     question.innerHTML = "Yeyy asikkk";
-    img.src =
+    image.src =
         "https://i.pinimg.com/564x/98/0d/42/980d4267e6ef39d72baff9984c0eafde.jpg";
 });
 
 NoBtn.addEventListener("click", () => {
     question.innerHTML = "Yaahhh kenapaa :(";
-    img.src =
+    image.src =
         "https://i.pinimg.com/564x/43/b5/73/43b573f37af779b43c4d493c7483cad6.jpg";
 });
